6642555ea663e036bcedf02a6be3b2fdd40fbeb3
GuFvr <gustavo.vfavero@gmail.com>
2024-03-05 22:51:04 -0300

    feat: Add participant form and button with console log action
    Created a form for adding participants and a button with an action (currently logging to console for testing purposes).

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,31 @@
-import { Text, View } from "react-native";
+import { Text, View, TextInput, TouchableOpacity } from "react-native";
 
 import { styles } from "./styles";
 
-export default function Home() {
+export function Home() {
+  function handleParticipantAdd() {
+    console.log("Você clicou no botão de adicionar");
+  }
+
   return(
     <View style={styles.container}>
-    <Text style={styles.eventName}>Nome do evento</Text>
+      <Text style={styles.eventName}>Nome do evento</Text>
 
-    <Text style={styles.eventDate}>Terça-feira, 5 de Março</Text>
+      <Text style={styles.eventDate}>Terça-feira, 5 de Março de 2024</Text>
+
+      <View style={styles.form}>
+        <TextInput 
+          style={styles.input} 
+          placeholder="Nome do participante"
+          placeholderTextColor="#6B6B6B"
+        />
+
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>
+            +
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -16,4 +16,32 @@ export const styles = StyleSheet.create({
     color: '#6B6B6B',
     fontSize: 16
   },
+  input: {
+    flex: 1,
+    height: 56,
+    backgroundColor: '#1F1E25',
+    borderRadius: 5,
+    color: '#FDFCFE',
+    padding: 16,
+    fontSize: 16,
+    marginRight: 12
+  },
+  buttonText: {
+    color: '#FFF',
+    fontSize: 24,
+  },
+  button: {
+    width: 56,
+    height: 56,
+    borderRadius: 5,
+    backgroundColor: '#31CF67',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  form: {
+    width: '100%',
+    flexDirection: 'row',
+    marginTop: 36,
+    marginBottom: 42,
+  }
 });
