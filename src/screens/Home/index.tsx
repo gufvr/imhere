4d98b2c98843d9a03a61bb3ecc157d066c05f10d
GuFvr <gustavo.vfavero@gmail.com>
2024-03-07 22:27:35 -0300

    feat: Implement adding participants dynamically
    Utilized the setParticipantName attribute to add a new participant when typing the name and clear the placeholder.

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -4,14 +4,16 @@ import { styles } from "./styles";
 
 import { Participant } from "../../components/Participant";
 export function Home() {
-  const [participants, setParticipants] = useState(['Thiago'],);
+  const [participants, setParticipants] = useState<string[]>([],);
+  const [participantName, setParticipantName] = useState('');
 
   function handleParticipantAdd() {
-    if (participants.includes('Gustavo')) {
-      return Alert.alert("Participante existe", "Já existe um participante na lista com esse nome.")
+    if (participants.includes(participantName)) {
+      return Alert.alert("Participante existente", "Já existe um participante na lista com esse nome.")
     }
 
-    setParticipants(prevState => [...prevState, 'Mika']);
+    setParticipants(prevState => [...prevState, participantName]);
+    setParticipantName('');
   }
 
   function handleParticipantRemove(name: string) {
@@ -38,6 +40,8 @@ export function Home() {
           style={styles.input} 
           placeholder="Nome do participante"
           placeholderTextColor="#6B6B6B"
+          onChangeText={setParticipantName}
+          value={participantName}
         />
 
         <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
